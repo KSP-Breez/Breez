23f35d8851de9264832bc04e7f42925cb7323312
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2021-01-30 01:07:36 +0300

    e

--- Imperium.py
+++ Imperium.py
@@ -1,9 +1,8 @@
 import argparse
 import os
 import sys
-import importlib
-
-
+from Colors import bcolors
+import progress
 
 nl = os.linesep
 
@@ -12,13 +11,15 @@ Parser = argparse.ArgumentParser(description="This will decide which file to com
 Parser.add_argument("Filename", metavar="filename", help="Filename of the file that you want to compile", type=str)
 args = Parser.parse_args()
 targetFile = args.Filename
+file_name, file_ext = os.path.splitext(targetFile)
 
-if os.path.isdir(targetFile) and targetFile.lower().endsWith(".ks"):
-    with open(targetFile, "w") as YamalCompiler:  
-        print(YamalCompiler.read())     
+if """os.path.isdir(targetFile)""" and file_ext == ".briz":
+    with open(targetFile, "r+") as YamalCompiler:  
+        breezContent = YamalCompiler.read
+            
 else:
     class InvalidFile_ERR(Exception):
-        def __init__(self, message=f"{nl}|File invalid{nl}|{nl}|Please enter a valid .ks file/path to .ks file"):
+        def __init__(self, message=f"{nl}|File invalid{nl}|{nl}|Please enter a valid path to .briz file"):
             self.message = message
             super().__init__(self.message)
     raise InvalidFile_ERR()
