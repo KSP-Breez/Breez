8c9e29b4dbb8e3d642724809f71ef68c66474c25
Delta-official <64544951+Darkuss420@users.noreply.github.com>
2021-02-14 23:51:19 +0300

    Small update

--- src/Yamal/EXT_Func.py
+++ src/Yamal/EXT_Func.py
@@ -6,10 +6,19 @@ from enum import Enum
 sender = os.path.basename(__file__)[:-3]
 unfixedTime = time.asctime(time.localtime(time.time()))
 
+class eventType(Enum):
+    ERROR = 1
+    START_END = 2
+    
+
 def Log(action, eventType):
     with open("log.ydl", "a") as YamalLogger:
+        for kind in tokenType:
+            # Relies on all keyword enum values being 1XX.
+            if kind.name.lower() == tokenText and kind.value >= 100 and kind.value < 400:
+                return kind
         YamalLogger.write(f"[{unfixedTime[4:]}]-[{sender}]-[{eventType}]: {action}.\n")
         
 def errorMes(self, message):
-    sys.exit(f"[{sender}] | ERROR: {message}")
     Log()
+    sys.exit(f"[{sender}] | ERROR: {message}")
